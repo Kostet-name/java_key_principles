cc68355fc355fd127edfff6911b4c6b977ecfa2e
Konstantin <kostenn07@mail.ru>
2023-04-09 19:58:44 +0300

    Initial commit

---
+++ src/main/java/FormDate.java
@@ -0,0 +1,5 @@
+public class FormDate {
+    public int day; // поле для числа
+    public int month; // поле для месяца
+    public int year; // поле для года
+}
---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name; // поле для имени
+    public String patronymic; // поле для отчества
+    public String surname; // поле для фамилии
+    public String passport; // серия и номер "4444 № 44444444"
+    public String phone; // номер "+7 (999)-999-99-99"
+    public FormDate birthday; // "13.06.1999"
+    public boolean subscription; // согласие
+}
